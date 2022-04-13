db49da5f79d03a1cbe91a46777652194f532b48d
Noah Taggart <noahtag@gmail.com>
2022-04-13 15:40:42 -0500

    added food to all classes, added function to display fed, added function to show animal

--- models/catfish.py
+++ models/catfish.py
@@ -2,9 +2,15 @@
 from datetime import date
 
 class Catfish:
-    def __init__(self, name, species, area):
+    def __init__(self, name, species, area, food):
         self.name = name
         self.species = species
         self.area = area
+        self.food = food
         self.swimming = True
         self.date_added = date.today()
+        
+    def feed(self):
+        return (f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
--- models/llama.py
+++ models/llama.py
@@ -1,12 +1,18 @@
 from datetime import date
 class Llama:
 
-    def __init__(self, name, species, area, shift):
+    def __init__(self, name, species, area, shift, food):
         # Establish the properties of each animal
         # with a default value
         self.name = name
         self.species = species
         self.area = area
         self.shift = shift
+        self.food = food
         self.walking = True
         self.date_added = date.today()
+        
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
